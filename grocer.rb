9b1cd8cb7468e0bc859b037fbfa1b2a0e7d0f450
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 17:27:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -78,7 +78,7 @@ def apply_coupons(cart, coupons)
         adjusted_cart[cart_index] = cart[cart_index]
       end
     else
-      adjusted_cart[cart_index] = cart[cart_index]
+      
     end
   
     cart_index += 1 
